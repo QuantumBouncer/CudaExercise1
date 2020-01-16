a6d270aa0cd6000acdc63cd1950f737f0fc49569
Hunter Hong <hong_1993@danusys.com>
2020-01-16 18:04:05 +0900

    saved date is : 2020-01-16  18:04

--- save.sh
+++ save.sh
@@ -2,5 +2,5 @@ NOW=$(date +"%Y-%m-%d  %H:%M")
 
 git add .
 git commit -m "saved date is : $NOW"
-git remote add origin https://github.com/QuantumBouncer/CudaExercise.git
+git remote add origin https://github.com/QuantumBouncer/CudaExercise1.git
 git push -u origin master
